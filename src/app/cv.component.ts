0a52f3ecf890c3a09ae92ab9e6aea5e19eabefe8
panda-codes <dedeomatse@gmail.com>
2024-05-25 18:49:02 +0100

    picture added

---
+++ src/app/cv.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'cv-body',
+    standalone:true,
+    templateUrl:'./cv.component.html',
+    styleUrl:'./cv.component.css'
+})
+
+
+export class CvComponent{}
